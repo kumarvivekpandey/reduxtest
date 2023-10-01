16814c061b7f6473c6a80f10c5323a8902d54d62
vivek kumar <vkpvivek1998@gmail.com>
2023-10-01 16:12:14 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[todos,Settodos]= useState([])
+  const[value, setInputVal] =useState("")
+  function writeTodo(e) {
+    setInputVal(e.target.value);
+  }
+  function addtodo(){
+   if(value !== ""){
+    Settodos((prevTodos) => [...prevTodos,value])
+   setInputVal("");
+   
+   }
+   
+ 
+  }
+  console.log(todos)
+  function deltodo(todosIndex){
+    Settodos((prevTodos) =>prevTodos.filter((prevTodos,prevTodosIndex)=>{
+      return prevTodosIndex !== todosIndex;
+    })
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1> toDo App</h1>
+     <input type="text" placeholder="enter the text" value={value} onChange={writeTodo}/>
+     <button className='button' onClick={addtodo}>+</button>
+     <div className="conatainer">
+
+     {
+  todos.map((todos,index)=>{
+    return(
+    <div className="conatiner textc" key={index}>
+         <p><input type="checkbox"/>{todos}
+         <button onClick={()=>deltodo(index)}>Delete</button></p> 
     </div>
+    )
+  })
+}
+</div>
+</div>
   );
 }
-
+ 
 export default App;
