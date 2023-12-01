7ec824a0c72ccb726b88fb09a84ce3a888db9063
Vivek Kumar <35356580+kumarvivekpandey@users.noreply.github.com>
2023-12-01 14:51:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,48 +1,59 @@
-
 import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const[todos,Settodos]= useState([])
-  const[value, setInputVal] =useState("")
+  const [todos, Settodos] = useState([]);
+  const [value, setInputVal] = useState("");
+
   function writeTodo(e) {
     setInputVal(e.target.value);
   }
-  function addtodo(){
-   if(value !== ""){
-    Settodos((prevTodos) => [...prevTodos,value])
-   setInputVal("");
-   
-   }
- 
+
+  function addtodo() {
+    if (value !== "") {
+      Settodos((prevTodos) => [...prevTodos, value]);
+      setInputVal("");
+    }
   }
-  console.log(todos)
-  function deltodo(todosIndex){
-    Settodos((prevTodos) =>prevTodos.filter((prevTodos,prevTodosIndex)=>{
-      return prevTodosIndex !== todosIndex;
-    })
+
+  function deltodo(todosIndex) {
+    Settodos((prevTodos) =>
+      prevTodos.filter((prevTodo, prevTodosIndex) => prevTodosIndex !== todosIndex)
     );
   }
+
+  function handleKeyPress(e) {
+    if (e.key === 'Enter') {
+      addtodo();
+    }
+  }
+
   return (
     <div className="App">
-     <h1> toDo App</h1>
-     <input type="text" placeholder="enter the text" value={value} onChange={writeTodo}/>
-     <button type="submit" className='button' onClick={addtodo}>+</button>
-     <div className="conatainer">
-
-     {
-  todos.map((todos,index)=>{
-    return(
-    <div className="conatiner textc" key={index}>
-         <p><input type="checkbox"/>{todos}
-         <button onClick={()=>deltodo(index)}>Delete</button></p> 
+      <h1>ToDo App</h1>
+      <input
+        type="text"
+        placeholder="Enter the text"
+        value={value}
+        onChange={writeTodo}
+        onKeyPress={handleKeyPress}
+      />
+      <button type="submit" className="button" onClick={addtodo}>
+        +
+      </button>
+      <div className="container">
+        {todos.map((todo, index) => (
+          <div className="container textc" key={index}>
+            <p>
+              <input type="checkbox" />
+              {todo}
+              <button onClick={() => deltodo(index)}>Delete</button>
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
-    )
-  })
-}
-</div>
-</div>
   );
 }
- 
+
 export default App;
